1496162a43c1f8fc9dceee18e0b75f2c7d6635c6
tawcent <rusx745@gmail.com>
2024-08-12 22:34:34 +0400

    Chess

---
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.chess;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.black.BishopBlack;
+
+import java.util.logging.XMLFormatter;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LogicTest {
+
+    @Test
+    public void whenMoveThenFigureNotFoundException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () ->
+            logic.move(Cell.C1, Cell.H6)
+        );
+        assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+    }
+
+    @Test
+    public void whenMoveOccupiedCellException()
+        throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    Logic logic = new Logic();
+    logic.add(new BishopBlack(Cell.A7));
+    logic.add(new BishopBlack(Cell.B8));
+    OccupiedCellException exception = assertThrows(OccupiedCellException.class, () ->
+            logic.move(Cell.A7, Cell.B8)
+    );
+    assertThat(exception.getMessage()).isEqualTo("Cell is occupied");
+    }
+
+    @Test
+    public void whenMoveImpossibleMoveException()
+        throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.A2));
+        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () ->
+                logic.move(Cell.A2, Cell.A4));
+        assertThat(exception.getMessage()).isEqualTo("Could not way by diagonal from %s to %s",
+                Cell.A2, Cell.A4);
+    }
+}
---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+
+import javax.swing.text.Position;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BishopBlackTest {
+    @Test
+    void whenPosition() {
+        Cell start = Cell.findBy(7, 2);
+        BishopBlack bishopBlack = new BishopBlack(start);
+        Cell finish = bishopBlack.position();
+        assertThat(finish). isEqualTo(start);
+    }
+
+    @Test
+    void whenCopy() {
+        Cell finish = Cell.findBy(7, 2);
+        BishopBlack bishopBlackCopy = new BishopBlack(finish);
+        assertThat(bishopBlackCopy.position()).isEqualTo(finish);
+    }
+
+    @Test
+    void whenWay() {
+        Cell start = Cell.findBy(7, 2);
+        BishopBlack bishopBlack = new BishopBlack(start);
+        Cell dest = Cell.findBy(2, 7);
+        Cell[] steps = bishopBlack.way(dest);
+        assertThat(steps).containsExactly(
+                Cell.findBy(6, 3),
+                Cell.findBy(5, 4),
+                Cell.findBy(4, 5),
+                Cell.findBy(3, 6),
+                Cell.findBy(2, 7)
+        );
+    }
+
+    @Test
+    void whenWayNotDiagonal() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () ->
+                bishopBlack.way(Cell.C2));
+        assertEquals("Could not way by diagonal from C1 to C2",
+                exception.getMessage()
+        );
+    }
+
+}
